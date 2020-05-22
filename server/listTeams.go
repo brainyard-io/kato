e7d5e6a4bed4b5ef01472511986848f596a16e40
nk-designz <nka@netzlink.com>
2020-05-22 17:13:45 +0200

    added the method name to the description for gofmt

--- server/listTeams.go
+++ server/listTeams.go
@@ -4,7 +4,7 @@ import (
 	pb "github.com/brainyard-io/kato/api"
 )
 
-// ListTeams returns a list of team ids
+// ListTeam returns a list of team ids
 func (s *Server) ListTeam(identifier *pb.Identifier, listTeamServer pb.Kato_ListTeamServer) error {
 	return nil
 }
